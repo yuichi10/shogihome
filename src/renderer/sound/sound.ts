752e4b530f983e57e25309fa38dd1f81946dfdd7
yuichi10 <y_084743@yahoo.co.jp>
2025-06-03 12:01:39 +0900

    add gold detail sound

--- src/renderer/sound/sound.ts
+++ src/renderer/sound/sound.ts
@@ -12,6 +12,21 @@ import {
 } from "tsshogi";
 import { XY } from "@/renderer/sound/xy";
 
+enum PieceOperation {
+  // 複数選択できる場合は、tier1を優先して選ぶ
+  AGARU = "上", // 上方向への移動 tier 1
+  YORU = "寄", // 左右方向への移動　tier 1
+  HIKU = "引", // 下方向への移動 tier 1
+  HIDARI = "左", // 左方向への移動
+  MIGI = "右", // 右方向への移動
+  SUGU = "直", // 真上方向への移動
+  HIDARI_AGARU = "左上", // 左上方向
+  HIDARI_HIKU = "左引", // 左下方向
+  MIGI_AGARU = "右上", // 右上方向
+  MIGI_HIKU = "右引", // 右下方向
+  UTU = "打", // 手駒から打つ場合
+}
+
 export class SoundManager {
   private synth: SpeechSynthesis;
   private audioContext: AudioContext;
@@ -138,7 +153,7 @@ export class SoundManager {
     return false;
   }
 
-  changeVoice(nextMove: Move): SoundType[] {
+  promoteVoice(nextMove: Move): SoundType[] {
     const voices: SoundType[] = [];
     if (nextMove.promote) {
       voices.push(SoundType.NARU);
@@ -168,6 +183,7 @@ export class SoundManager {
     return voices;
   }
 
+  // moveDirectionに入っているXY毎の方向にいるコマを調査して、それが自分と同じコマだったらその方向を返す。
   aroundPieceDirection(movableDirection: XY[], record: ImmutableRecord, nextMove: Move): XY[] {
     const aroundPieceDirection: XY[] = [];
     const nextPieceType = nextMove.pieceType;
@@ -176,7 +192,7 @@ export class SoundManager {
     }
 
     for (const direction of movableDirection) {
-      const fromAroundSquare = nextMove.to.neighbor(direction.X, direction.Y);
+      const fromAroundSquare = nextMove.to.neighbor(direction.x, direction.y);
       const piece = record.position.board.at(fromAroundSquare);
 
       if (
@@ -192,12 +208,13 @@ export class SoundManager {
     return aroundPieceDirection;
   }
 
+  // 複数距離をいくような駒がlongRangePatternの方向に複数進んだ際に、nextMoveに来れる自分と同じ駒がいるか、調査。
   longRangeSamePieceSearch(longRangePattern: XY[], record: ImmutableRecord, nextMove: Move): XY[] {
     const aroundPieceDirection: XY[] = [];
 
     for (const pattern of longRangePattern) {
       for (let i = 0; i < 10; i++) {
-        const searchSquare = nextMove.to.neighbor(pattern.X * i, pattern.Y * i);
+        const searchSquare = nextMove.to.neighbor(pattern.x * i, pattern.y * i);
         const piece = record.position.board.at(searchSquare);
         if (
           searchSquare.file < 1 ||
@@ -225,26 +242,206 @@ export class SoundManager {
     return aroundPieceDirection;
   }
 
+  pieceOperationCanddate(nextMove: Move, nextToFromOperation: XY): PieceOperation[] {
+    const po: PieceOperation[] = [];
+
+    if (Object.values(PieceType).includes(nextMove.from as PieceType)) {
+      po.push(PieceOperation.UTU);
+      return po;
+    }
+
+    // 左と右に関しては左に移動ではなく左にあるものが右にいくという形なので、Hidariに行くときはMIGIを設定している。
+    const from = nextMove.to.neighbor(nextToFromOperation.x, nextToFromOperation.y);
+    const to = nextMove.to;
+    const ue = from.neighbor(XY.UE(nextMove.color).x, XY.UE(nextMove.color).y);
+    const hidari = from.neighbor(XY.MIGI(nextMove.color).x, XY.MIGI(nextMove.color).y);
+    const migi = from.neighbor(XY.HIDARI(nextMove.color).x, XY.HIDARI(nextMove.color).y);
+    const hiku = from.neighbor(XY.HIKU(nextMove.color).x, XY.HIKU(nextMove.color).y);
+    const hidariAgaru = from.neighbor(
+      XY.MIGI_AGARU(nextMove.color).x,
+      XY.MIGI_AGARU(nextMove.color).y,
+    );
+    const migiAgaru = from.neighbor(
+      XY.HIDARI_AGARU(nextMove.color).x,
+      XY.HIDARI_AGARU(nextMove.color).y,
+    );
+    const hidariHiku = from.neighbor(
+      XY.MIGI_HIKU(nextMove.color).x,
+      XY.MIGI_HIKU(nextMove.color).y,
+    );
+    const migiHiku = from.neighbor(
+      XY.HIDARI_HIKU(nextMove.color).x,
+      XY.HIDARI_HIKU(nextMove.color).y,
+    );
+
+    // 上
+    if (ue.rank === to.rank) {
+      po.push(PieceOperation.AGARU);
+    }
+    // 寄る
+    if (
+      (hidari.file === to.file && hidari.rank === to.rank) ||
+      (migi.file === to.file && migi.rank === to.rank)
+    ) {
+      po.push(PieceOperation.YORU);
+    }
+    // 引く
+    if (hiku.rank === to.rank) {
+      po.push(PieceOperation.HIKU);
+    }
+    // 左
+    if (hidari.file === to.file) {
+      po.push(PieceOperation.HIDARI);
+    }
+    // 右
+    if (migi.file === to.file) {
+      po.push(PieceOperation.MIGI);
+    }
+    // 直ぐ
+    if (ue.rank === to.rank && ue.file === to.file) {
+      po.push(PieceOperation.SUGU);
+    }
+    // 左上
+    if (hidariAgaru.rank === to.rank && hidariAgaru.file === to.file) {
+      po.push(PieceOperation.HIDARI_AGARU);
+    }
+    // 左引く
+    if (hidariHiku.rank === to.rank && hidariHiku.file === to.file) {
+      po.push(PieceOperation.HIDARI_HIKU);
+    }
+    // 右上
+    if (migiAgaru.rank === to.rank && migiAgaru.file === to.file) {
+      po.push(PieceOperation.MIGI_AGARU);
+    }
+    // 右引く
+    if (migiHiku.rank === to.rank && migiHiku.file === to.file) {
+      po.push(PieceOperation.MIGI_HIKU);
+    }
+    return po;
+  }
+
+  operationCount(nextMove: Move, moveOperations: XY[]): Map<PieceOperation, number> {
+    const count: Map<PieceOperation, number> = new Map<PieceOperation, number>();
+    for (let i = 0; i < moveOperations.length; i++) {
+      const ops = this.pieceOperationCanddate(nextMove, moveOperations[i]);
+
+      for (const op of ops) {
+        const opCount = count.get(op);
+        if (opCount === undefined) {
+          count.set(op, 1);
+        } else {
+          count.set(op, opCount + 1);
+        }
+      }
+    }
+    return count;
+  }
+
+  choiceOperation(operations: PieceOperation[]): PieceOperation {
+    // 複数選択できる場合は、tier1を優先して返す
+    // AGARU = "上", // 上方向への移動 tier 1
+    // YORU = "寄", // 左右方向への移動　tier 1
+    // HIKU = "引", // 下方向への移動 tier 1
+    if (operations.length === 1) {
+      return operations[0];
+    }
+    if (operations.includes(PieceOperation.AGARU)) {
+      return PieceOperation.AGARU;
+    } else if (operations.includes(PieceOperation.YORU)) {
+      return PieceOperation.YORU;
+    } else if (operations.includes(PieceOperation.HIKU)) {
+      return PieceOperation.HIKU;
+    }
+    return operations[0];
+  }
+
+  // piceOperationによって音を変える。
+  pieceOperationToSoundType(operation: PieceOperation): SoundType[] {
+    const voices: SoundType[] = [];
+    switch (operation) {
+      case PieceOperation.AGARU:
+        voices.push(SoundType.AGARU);
+        break;
+      case PieceOperation.YORU:
+        voices.push(SoundType.YORU);
+        break;
+      case PieceOperation.HIKU:
+        voices.push(SoundType.HIKU);
+        break;
+      case PieceOperation.HIDARI:
+        voices.push(SoundType.HIDARI);
+        break;
+      case PieceOperation.MIGI:
+        voices.push(SoundType.MIGI);
+        break;
+      case PieceOperation.SUGU:
+        voices.push(SoundType.SUGU);
+        break;
+      case PieceOperation.HIDARI_AGARU:
+        voices.push(SoundType.HIDARI);
+        voices.push(SoundType.AGARU);
+        break;
+      case PieceOperation.HIDARI_HIKU:
+        voices.push(SoundType.HIDARI);
+        voices.push(SoundType.HIKU);
+        break;
+      case PieceOperation.MIGI_AGARU:
+        voices.push(SoundType.MIGI);
+        voices.push(SoundType.AGARU);
+        break;
+      case PieceOperation.MIGI_HIKU:
+        voices.push(SoundType.MIGI);
+        voices.push(SoundType.HIKU);
+        break;
+      case PieceOperation.UTU:
+        voices.push(SoundType.UTU);
+        break;
+    }
+    return voices;
+  }
+
   goldOtherMoves(record: ImmutableRecord, nextMove: Move): SoundType[] {
+    // 先手と後手で　上とか下の概念が変わる
     const voices: SoundType[] = [];
 
+    // TODO: ここはあくまで相対なので考える必要があるかも。
+    // TODO: 先手と後手でneibourの向きが違うかもしれない。これは要調査。
     const movableDirection: XY[] = [
-      new XY(1, 0), // 左
-      new XY(-1, 0), // 右
-      new XY(0, 1), // 下
-      new XY(0, -1), // 上
-      new XY(1, 1), // 左下
-      new XY(-1, 1), // 右下
+      XY.HIDARI(nextMove.color),
+      XY.MIGI(nextMove.color),
+      XY.HIKU(nextMove.color),
+      XY.UE(nextMove.color),
+      XY.HIDARI_HIKU(nextMove.color),
+      XY.MIGI_HIKU(nextMove.color),
     ];
     const aroundPieceDirection = this.aroundPieceDirection(movableDirection, record, nextMove);
 
     // 他にも動かせる駒があるとき
     if (aroundPieceDirection.length > 0) {
       //　打つ場合
-      if (nextMove.from === nextMove.pieceType) {
+      if (!(nextMove.from instanceof Square)) {
         voices.push(SoundType.UTU);
         return voices;
       }
+
+      // 自身の行動方法
+      const selfMove: XY = new XY(
+        (nextMove.from.file - nextMove.to.file) * -1,
+        nextMove.from.rank - nextMove.to.rank,
+      );
+
+      const selfOperation = this.pieceOperationCanddate(nextMove, selfMove);
+      const otherOperationCount = this.operationCount(nextMove, aroundPieceDirection);
+
+      const operationFinalList: PieceOperation[] = [];
+      for (const ops of selfOperation) {
+        if (otherOperationCount.get(ops) === undefined) {
+          operationFinalList.push(ops);
+        }
+      }
+
+      const operationExplain = this.choiceOperation(operationFinalList);
+      voices.push(...this.pieceOperationToSoundType(operationExplain));
     }
     return voices;
   }
@@ -353,7 +550,7 @@ export class SoundManager {
     return voices;
   }
 
-  moveDetailVoice(record: ImmutableRecord): SoundType[] {
+  operationVoice(record: ImmutableRecord): SoundType[] {
     const voices: SoundType[] = [];
     const nextMove = record.current.next?.move as Move | null;
     if (!nextMove) {
@@ -406,8 +603,8 @@ export class SoundManager {
     voices.push(...this.turnVoice(nextMove));
     voices.push(...this.getPlaceVoice(currentMove, nextMove));
     voices.push(...this.getPieceVoice(nextMove));
-    voices.push(...this.changeVoice(nextMove));
-    voices.push(...this.moveDetailVoice(record));
+    voices.push(...this.promoteVoice(nextMove));
+    voices.push(...this.operationVoice(record));
 
     return voices;
   }
--- src/renderer/sound/xy.ts
+++ src/renderer/sound/xy.ts
@@ -1,18 +1,61 @@
+import { Color } from "tsshogi";
+
 export class XY {
+  static UP = new XY(0, -1);
+  static DOWN = new XY(0, 1);
+  static LEFT = new XY(-1, 0);
+  static RIGHT = new XY(1, 0);
+  static UP_LEFT = new XY(-1, -1);
+  static UP_RIGHT = new XY(1, -1);
+  static DOWN_LEFT = new XY(-1, 1);
+  static DOWN_RIGHT = new XY(1, 1);
   private _x: number;
   private _y: number;
-  constructor(
-    public x: number,
-    public y: number,
-  ) {
+  constructor(x: number, y: number) {
     this._x = x;
     this._y = y;
   }
 
-  get X(): number {
+  get x(): number {
     return this._x;
   }
-  get Y(): number {
+  get y(): number {
     return this._y;
+  }
+
+  dx_dy(): { dx: number; dy: number } {
+    return { dx: this.x, dy: this.y };
+  }
+
+  static UE(color: Color): XY {
+    return color === Color.BLACK ? this.UP : this.DOWN;
+  }
+
+  static HIDARI(color: Color): XY {
+    return color === Color.BLACK ? this.LEFT : this.RIGHT;
+  }
+
+  static MIGI(color: Color): XY {
+    return color === Color.BLACK ? this.RIGHT : this.LEFT;
+  }
+
+  static HIKU(color: Color): XY {
+    return color === Color.BLACK ? this.DOWN : this.UP;
+  }
+
+  static HIDARI_AGARU(color: Color): XY {
+    return color === Color.BLACK ? this.UP_LEFT : this.DOWN_RIGHT;
+  }
+
+  static HIDARI_HIKU(color: Color): XY {
+    return color === Color.BLACK ? this.DOWN_LEFT : this.UP_RIGHT;
+  }
+
+  static MIGI_AGARU(color: Color): XY {
+    return color === Color.BLACK ? this.UP_RIGHT : this.DOWN_LEFT;
+  }
+
+  static MIGI_HIKU(color: Color): XY {
+    return color === Color.BLACK ? this.DOWN_RIGHT : this.UP_LEFT;
   }
 }
